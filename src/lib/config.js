38e3fcfb8aebb43957ce2b6062849b8ec0a097e1
Jeki Maulana <jekibus@gmail.com>
2023-01-16 10:09:37 +0700

    prepare push

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,6 +1,6 @@
 // export const SOCKET_ENDPOINT = "http://localhost:3000";
-export const SOCKET_ENDPOINT = "";
+export const SOCKET_ENDPOINT = "https://socket.ismartglobal.id";
 
 export const DEFAULT_EVENT = "msg";
 
-export const DEFAULT_WA = "";
+export const DEFAULT_WA = "6281310746202";
